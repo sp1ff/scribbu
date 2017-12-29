f3d5d185e08e806818190f801d9773acf98da090
Michael Herstine <sp1ff@pobox.com>
2017-12-29 10:29:27 -0800

    `test-cleanup-encoded-by' passing.
    Now scripting scribbu from Scheme to update tags!

--- test/test-cleanup-encoded-by.scm
+++ test/test-cleanup-encoded-by.scm
@@ -5,75 +5,77 @@
 (setlocale LC_ALL "")
 
 (define (report-on-encoded-by track)
+  "Utility routine for evaluating the 'encoded by' attribute of TRACK.
+
+This function is for testing & debugging purposes. It will simply print the track
+path, the ID3v1 comment (if any) and the ID3v2 TENC frame (if any)."
   (format #t "~s: " (scribbu/get-path track))
   (if (scribbu/has-id3v1-tag track)
 	  (format #t "ID3v1 comment: ~s" (scribbu/get-id3v1-string track 'comment))
 	  (display "<no ID3v1 match>"))
   (let ((num-tags (scribbu/get-id3v2-tag-count track)))
-	(do ((i 0 (1+ i)))
-		((>= i num-tags))
-	  (if (scribbu/has-id3v2-attribute track i 'encoded-by)
-		  (format #t ", ~d/encoded-by: ~s" i
-				  (scribbu/get-id3v2-attribute track i 'encoded-by)))))
+	  (do ((i 0 (1+ i)))
+		    ((>= i num-tags))
+	    (if (scribbu/has-id3v2-attribute track i 'encoded-by)
+		      (format #t ", tag ~d/encoded-by: ~s" i
+				          (scribbu/get-id3v2-attribute track i 'encoded-by)))))
   (format #t "\n"))
 
-;; If `track' has an ID3v1 comment field matching /.*winamp.*/:
+(define (cleanup-encoded-by track)
+  "Clean-up the 'encoded-by' attribute of TRACK.
+  
+If TRACK does not have an ID3v1 comment field matching /.*winamp.*/,
+do nothing.
 
-;;   If `track' has an ID3v2 tag without a TENC frame, add a TENC frame of "Winamp"
-;;   else if `track' has no ID3v2 tag. create one with only a TENC frame of "Winamp"
-;;   else print a warning consisting of the TENC frames in the extant ID3v2 frames.
-(define (cleanup-encoded-by-1 track)
-  (format #t "cleanup-encoded-by-1: ~s\n" (scribbu/get-path track))
+Else, if TRACK has an ID3v2 tag without a TENC frame, add a TENC frame
+of 'Winamp'.  If TRACK has no ID3v2 tag. create one with only a TENC
+frame of 'Winamp'. Otherwise, print a warning consisting of the TENC
+frames in the extant ID3v2 frames."
+  
   (if (scribbu/has-id3v1-tag track)
-	  (let ((r (make-regexp ".*winamp.*" regexp/icase)))
-		(format #t "~s => ~s\n"
-				(scribbu/get-id3v1-string track 'comment)
-				(regexp-exec r (scribbu/get-id3v1-string track 'comment)))
-		(if (regexp-exec r (scribbu/get-id3v1-string track 'comment))
-			(begin
-			  (display "... evaluated as True!\n")
-			  (let ((num-tags (scribbu/get-id3v2-tag-count track)))
-				(if (eq? num-tags 0)
-					(begin
-					  (display "... no ID3v2 tags\n")
-					  (scribbu/make-id3v2-tag track 0)
-					  (scribbu/set-id3v2-attribute track 0 'encoded-by "Winamp")
-					  (format #t "Writing ~s...\n"
-							  (string-join (list (basename (scribbu/get-path track)) "out") "."))
-					  (scribbu/write-id3v2-tag
-					   track 0
-					   (string-join (list (basename (scribbu/get-path track)) "out") "."))
-					  (format #t "Writing ~s...done.\n"
-							  (string-join (list (basename (scribbu/get-path track)) "out") ".")))
-					(let ((encoders '()))
-					  (begin
-						(format #t "... ~d ID3v2 tags\n" num-tags)
-						(do ((i 0 (1+ i)))
-							((>= i num-tags))
-						  (if (scribbu/has-id3v2-attribute track i 'encoded-by)
-							  (set!
-							   encoders
-							   (cons (scribbu/get-id3v2-attribute track i 'encoded-by) encoders))
-							  (begin
-							  (scribbu/set-id3v2-attribute track i 'encoded-by "Winamp")
-							  (scribbu/write-id3v2-tag
-							   track i
-							   (string-join (list
-											 (basename (scribbu/get-path track))
-											 (number->string i)
-											 "out") "."))))))
-					  (if (eq? num-tags (length encoders))
-						  (format #t "~s: already encoded by ~s\n"
-								  (scribbu/get-path track) encoders))))))))))
+	    (let ((r (make-regexp ".*winamp.*" regexp/icase)))
+		    (if (regexp-exec r (scribbu/get-id3v1-string track 'comment))
+			      (begin
+			        (let ((num-tags (scribbu/get-id3v2-tag-count track)))
+				        (if (eq? num-tags 0)
+					          (begin
+					            (scribbu/make-id3v2-tag track 0)
+					            (scribbu/set-id3v2-attribute track 0 'encoded-by "Winamp")
+					            (scribbu/write-id3v2-tag
+					             track 0
+					             (string-join (list (basename (scribbu/get-path track)) "out") ".")))
+					          (let ((encoders '()))
+					            (begin
+						            (do ((i 0 (1+ i)))
+							              ((>= i num-tags))
+						              (if (scribbu/has-id3v2-attribute track i 'encoded-by)
+							                (set!
+							                 encoders
+							                 (cons (scribbu/get-id3v2-attribute track i 'encoded-by) encoders))
+							                (begin
+							                  (scribbu/set-id3v2-attribute track i 'encoded-by "Winamp")
+							                  (scribbu/write-id3v2-tag
+							                   track i
+							                   (string-join (list
+											                         (basename (scribbu/get-path track))
+											                         (number->string i)
+											                         "out") "."))))))
+                      (if (eq? num-tags (length encoders))
+                          (format #t "~s: already encoded by ~s\n"
+                                  (scribbu/get-path track) encoders))))))))))
 
 (define (main dir)
+  "Cleanup the 'Encoded By' attribute for all tags in DIR."
   (let ((data (string-join (list dir "data") "/" 'infix)))
-	(scribbu/with-track-in data report-on-encoded-by)
-	(scribbu/with-track-in data cleanup-encoded-by-1)))
+    (display "Report:\n=======\n")
+	  (scribbu/with-track-in data report-on-encoded-by)
+    (display "\n")
+    (display "Cleanup:\n========\n")
+	  (scribbu/with-track-in data cleanup-encoded-by)))
 
 (let ((cl (cdr (command-line))))
   (if (= 1 (length cl))
-	  (main (car cl))
-	  (begin
-		(format #t "Usage: test-cleanup-encoded-by.scm ${srcdir}\n")
-		(exit 2))))
+	    (main (car cl))
+	    (begin
+		    (format #t "Usage: test-cleanup-encoded-by.scm ${srcdir}\n")
+		    (exit 2))))
